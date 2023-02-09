1911320b51a3f425ce77cce24f7eb6e2c67cf571
Oirad7 <60646094+Oirad7@users.noreply.github.com>
2023-02-09 11:28:52 -0300

    adicionados endpoints delete e put

--- index.js
+++ index.js
@@ -50,6 +50,71 @@ app.get("/game/:id", (req, res) => {
     }
 })
 
+app.post("/game", (req, res) => {
+    var {title, price, year} = req.body;
+
+    DB.games.push({
+        id: 2323,
+        title,
+        price,
+        year
+    });
+
+    res.sendStatus(200);
+});
+
+app.delete("/game/:id", (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        var index = DB.games.findIndex(g => g.id == id);
+
+        if(index == -1){
+            res.sendStatus(404);
+        }else{
+            DB.games.splice(index,1);
+            res.sendStatus(200)
+        }
+    }
+});
+
+app.put("/game/:id", (req, res) => {
+
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+
+        var id = parseInt(req.params.id);
+
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+
+            var {title, price, year} = req.body;
+
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+            
+            if(year != undefined){
+                game.year = year;
+            }         
+            
+            res.sendStatus(200);
+
+        }else {
+            res.sendStatus(404);
+        }
+    }
+});
+
+
+
 app.listen(45678,() => {
     console.log("API rodando!");
 })
